13ad16ce64d7d3c5cc260fc8a3f503ea9f38267d
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-11-13 11:40:37 +0000

    Fix bug, checking email type in register form

--- webApp/src/Form/RegistrationFormType.php
+++ webApp/src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,7 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('name')
             ->add('surname')
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
