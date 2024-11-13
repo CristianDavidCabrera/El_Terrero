579c6b443295a94c3b54aacc915238b4afdafc5a
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2024-11-13 16:57:01 +0000

    Create CRUD Team

---
+++ webApp/src/Form/TeamType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Club;
+use App\Entity\Team;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('division')
+            ->add('id_fighter')
+            ->add('id_club', EntityType::class, [
+                'class' => Club::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Team::class,
+        ]);
+    }
+}
